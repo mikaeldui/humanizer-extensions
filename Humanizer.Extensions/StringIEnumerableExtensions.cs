24516b6ede50c27e37158bcbb07b4690d639714a
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-13 01:34:18 +0100

    Added IEnumerable extensions for String.

---
+++ Humanizer.Extensions/StringIEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Humanizer
+{
+    public static class StringIEnumerableExtensions
+    {
+        public static IEnumerable<string> Pluralize(this IEnumerable<string> source, bool inputIsKnownToBeSingular = true) =>
+            source.Select(s => s.Pluralize(inputIsKnownToBeSingular));
+
+        public static IEnumerable<string> Singularize(this IEnumerable<string> source, bool inputIsKnownToBePlural = true, bool skipSimpleWords = false) =>
+            source.Select(s => s.Singularize(inputIsKnownToBePlural, skipSimpleWords));
+    }
+}
